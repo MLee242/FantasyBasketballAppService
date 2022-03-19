8302e2186a49f34260c27e8a72e0cd3136c99e76
moon lee <buddybuddymoon@gmail.com>
2022-03-19 12:33:39 -0400

    Initializing Backend Service

---
+++ src/main/java/yahoofantasy/FantasyBasketballServiceApplication.java
@@ -0,0 +1,13 @@
+package yahoofantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyBasketballServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyBasketballServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yahoofantasy/FantasyBasketballServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package yahoofantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyBasketballServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
